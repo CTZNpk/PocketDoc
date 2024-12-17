138a2047723166642d2f28f20b5878fd964851f0
haider <haidersultan7675@gmail.com>
2024-12-18 00:18:52 +0500

    Incomplete Cards

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { useEffect, useRef } from "react";
-
+import styled from 'styled-components';
 
 export default function Homepage() {
   return (
     <div className="bg-black text-white ">
       <IntroScreen />
       <SummarizeDocuments />
+      <GenerateQuiz />
       <MultipleSelection />
-      <section className="min-h-screen"></section>
-
     </div>
   );
 }
@@ -170,7 +169,7 @@ function SummarizeDocuments() {
   );
 };
 
-function MultipleSelection() {
+function GenerateQuiz() {
   useEffect(() => {
     const script = document.createElement('script');
     script.src = "https://unpkg.com/@lottiefiles/lottie-player@2.0.8/dist/lottie-player.js";
@@ -223,4 +222,90 @@ function MultipleSelection() {
     </section>
   );
 }
+
+function MultipleSelection() {
+  return (
+    <section className="min-h-[75vh]">
+      <div className="flex bg-cyan-600 my-[20vh] min-h-[15vh] items-center justify-center">
+        <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold ">
+          Explore our tools and become more productive
+        </h1>
+
+
+      </div>
+      <div className="flex flex-col-reverse lg:flex-row px-0 mx-auto sm:px-6 lg:px-8 max-w-7xl items-center justify-evenly">
+        <Card text="Summarize Documents" />
+        <Card text="Query Based Summary" />
+        <Card text="Generate Quiz" />
+
+      </div>
+    </section>
+
+  );
+}
+
+
+const Card = ({ text }) => {
+  return (
+    <StyledWrapper>
+      <div className="card ">{text}</div>
+    </StyledWrapper>
+  );
+}
+
+const StyledWrapper = styled.div`
+  .card {
+    position: relative;
+    width: 220px;
+    height: 320px;
+    background: #06b6d4;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 25px;
+    font-weight: bold;
+    border-radius: 15px;
+    text-align:center;
+    cursor: pointer;
+  }
+
+  .card::before,
+  .card::after {
+    position: absolute;
+    content: "";
+    width: 20%;
+    height: 20%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 25px;
+    font-weight: bold;
+    background-color: lightblue;
+    transition: all 0.5s;
+  }
+
+  .card::before {
+    top: 0;
+    right: 0;
+    border-radius: 0 15px 0 100%;
+  }
+
+  .card::after {
+    bottom: 0;
+    left: 0;
+    border-radius: 0 100%  0 15px;
+  }
+
+  .card:hover::before,
+  .card:hover:after {
+    width: 100%;
+    height: 100%;
+    border-radius: 15px;
+    transition: all 0.5s;
+  }
+
+  .card:hover:after {
+    content: "HELLO";
+  }`;
+
 
